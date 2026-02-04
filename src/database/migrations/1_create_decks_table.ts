e986395c3096e359e02fde2e534f66c9ad384f2a
Jonathan150408 <jonathan.junod@eduvaud.ch>
2026-02-04 15:26:25 +0100

    feat(migrations) : ajout des fichiers
    [10][Done]

---
+++ src/database/migrations/1_create_decks_table.ts
@@ -0,0 +1,23 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'decks'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+
+      // nom du deck unique et non-null
+      table.string('name').notNullable().unique()
+      //description du deck
+      table.string('description')
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
---
+++ src/database/migrations/2_create_cards_table.ts
@@ -0,0 +1,23 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'cards'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+
+      // question non-nulle et unique
+      table.string('question').notNullable().unique()
+      //réponse non-nulle et unique
+      table.string('answer').notNullable().unique()
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
