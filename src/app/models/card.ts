ff36e9a3d79d39f7041f02d9e56f1bf83fcf3ce7
Jonathan150408 <jonathan.junod@eduvaud.ch>
2026-02-04 15:33:28 +0100

    feat(models) : ajout des models
    [5][Done]

---
+++ src/app/models/card.ts
@@ -0,0 +1,20 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+
+export default class Card extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  //champ question
+  @column()
+  declare question: String
+  //champ réponse
+  @column()
+  declare answer: String
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
---
+++ src/app/models/deck.ts
@@ -0,0 +1,20 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+
+export default class Deck extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  //champ nom
+  @column()
+  declare name: String
+  //champ description
+  @column()
+  declare description: String
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
