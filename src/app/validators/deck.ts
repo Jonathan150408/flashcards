a784cabc75676340ae1d5c3fbf29c5032e41ab62
Jonathan150408 <jonathan.junod@eduvaud.ch>
2026-02-25 15:52:47 +0100

    doc(analyse fonctionnelle) : mise à jour et ajout de min 10 caractères pour valider un deck
    [5][Done]

--- src/app/validators/deck.ts
+++ src/app/validators/deck.ts
@@ -3,7 +3,7 @@ const deckValidator = vine.compile(
   vine.object({
     //validation des champs (varchar 255 non-null)
     name: vine.string().trim().minLength(1).maxLength(255),
-    description: vine.string().trim().minLength(1).maxLength(255),
+    description: vine.string().trim().minLength(10).maxLength(255),
   })
 )
 export { deckValidator }
