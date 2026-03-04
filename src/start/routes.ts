6faa256aa38a26c2bb31668bf6bb51ec17573ef9
Jonathan150408 <jonathan.junod@eduvaud.ch>
2026-03-04 13:36:22 +0100

    feat(routes pour les cartes) :  préparer et adapter les routes
    [5][Done]

--- src/start/routes.ts
+++ src/start/routes.ts
@@ -11,8 +11,9 @@ import CardsController from '#controllers/cards_controller'
 import DecksController from '#controllers/decks_controller'
 import router from '@adonisjs/core/services/router'
 
+//--------------- partie pour les decks ---------------
+//accueil
 router.get('/', [DecksController, 'index']).as('home')
-
 //form création de decks
 router.get('/decks/create', [DecksController, 'create']).as('decks_create')
 //enregistrement du deck
@@ -24,3 +25,13 @@ router.get('/decks/:id/destroy', [DecksController, 'destroy']).as('decks_destroy
 //modifier un deck
 router.get('/decks/:id/edit', [DecksController, 'edit']).as('decks_edit') //1. afficher le form
 router.post('/decks/:id/update', [DecksController, 'update']).as('decks_update') //2. valider et enregistrer
+
+//--------------- partie pour les cartes ---------------
+//création d'une carte
+router.get('/cards/create', [CardsController, 'create']).as('cards_create') //1. form de création
+router.post('/cards/store', [CardsController, 'store']).as('cards_store') //2. validation et création
+//supprimer un carte
+router.get('/cards/:id/destroy', [CardsController, 'destroy']).as('cards_destroy')
+//modifier un carte
+router.get('/cards/:id/edit', [CardsController, 'edit']).as('cards_edit') //1. afficher le form
+router.post('/cards/:id/update', [CardsController, 'update']).as('cards_update') //2. valider et enregistrer
